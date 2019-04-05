020a1dd398963ef550b0db246f3d630cb188fd5e
Jules Kouatchou <Jules.Kouatchou@nasa.gov>
2019-04-05 09:33:46 -0400

    new materials

---
+++ pyCode/average_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Get three test score
+try:
+   round1 = int(raw_input("Enter score for round 1: "))
+   round2 = int(raw_input("Enter score for round 2: "))
+   round3 = int(raw_input("Enter score for round 3: "))
+except:
+   round1 = int(input("Enter score for round 1: "))
+   round2 = int(input("Enter score for round 2: "))
+   round3 = int(input("Enter score for round 3: "))
+   
+# Calculate the average
+average = (round1 + round2 + round3) / 3
+
+# Print out the test score
+print("the average score is: ", average )
